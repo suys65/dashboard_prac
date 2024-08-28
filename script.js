79e7535c1813d4b22d9a2d9b7945767f53ce533b
suys65 <suysinu65@gmail.com>
2024-08-29 06:10:49 +0900

    기능

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
-function loadDashboard(dashboardName) {
-    // Tableau 대시보드 컨테이너 및 관련 요소 참조
-    const tableauContainer = document.getElementById('vizContainer');
-    const tableauObject = tableauContainer.getElementsByTagName('object')[0];
-    let nameValue = '';
+let viz; // 전역 변수로 viz 객체를 저장
 
-    // 대시보드 이름에 따라 Tableau 대시보드 URL 설정
-    if (dashboardName === 'dashboard1') {
-        nameValue = '_17248735198700/sheet0';
-    } else if (dashboardName === 'dashboard2') {
-        nameValue = '_17248735198701/sheet0';
-    }
-
-    // Tableau Object 요소의 name 속성 변경
-    tableauObject.querySelector("param[name='name']").value = nameValue;
-
-    // Tableau API 스크립트가 이미 로드되어 있는지 확인
+function loadDashboard() {
+    const divElement = document.getElementById('vizContainer');
+    const vizElement = divElement.getElementsByTagName('object')[0];
+    
+    // 대시보드 URL 설정
+    const dashboardURL = "https://public.tableau.com/views/_17248735198700/sheet0?:language=ko-KR&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+    
+    // URL 업데이트
+    vizElement.querySelector("param[name='name']").value = "_17248735198700/sheet0";
+    
+    // Tableau API 스크립트 로드
     const existingScript = document.querySelector("script[src='https://public.tableau.com/javascripts/api/viz_v1.js']");
     if (!existingScript) {
         const scriptElement = document.createElement('script');
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-        tableauObject.parentNode.insertBefore(scriptElement, tableauObject);
+        scriptElement.onload = function() {
+            new tableau.Viz(divElement, dashboardURL, {
+                width: '100%',
+                height: '100%'
+            });
+        };
+        document.body.appendChild(scriptElement);
     } else {
-        // 스크립트가 이미 로드된 경우, 대시보드 리로드
-        tableauObject.style.display = 'block';
-        tableauObject.parentNode.removeChild(existingScript);
-        tableauObject.parentNode.insertBefore(existingScript, tableauObject);
+        // 이미 스크립트가 로드된 경우
+        new tableau.Viz(divElement, dashboardURL, {
+            width: '100%',
+            height: '100%'
+        });
     }
+}
+
+
+function createViz(divElement, dashboardURL) {
+    // 기존 대시보드가 있다면 제거
+    if (viz) {
+        viz.dispose();
+    }
+
+    // 새로운 대시보드 생성
+    viz = new tableau.Viz(divElement, dashboardURL, {
+        width: '100%',
+        height: '100%'
+    });
+}
+
+function removeDashboard() {
+    const divElement = document.getElementById('vizContainer');
+
+    // 대시보드 제거
+    if (viz) {
+        viz.dispose();
+        viz = null;
+    }
+    
+    // 컨테이너에서 대시보드 내용 지우기
+    divElement.innerHTML = '';
 }
